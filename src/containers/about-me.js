ebd4402faf6245031c5425374282b6dcd3b13c0f
Yasin ATEŞ <yasinatesim@gmail.com>
2021-06-07 22:42:46 +0300

    feat: add absolute import width next new version

--- src/containers/about-me.js
+++ src/containers/about-me.js
@@ -7,7 +7,7 @@ function AboutMe() {
 	return (
 		<Text>
 			This is index page and this text is global css usage example
-				
+
 			<br/>
 			<div className={styles.aboutMe}>This text is CSS Module example</div>
 		</Text>
